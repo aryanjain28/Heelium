797119d2821ad9295153b5afc65d56fbce0f1918
Aryan Jain <aryanjn807@gmail.com>
2021-10-21 23:52:38 +0530

    ->added profile image dropdown

--- src/Components/AppHeaderComponent/index.jsx
+++ src/Components/AppHeaderComponent/index.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
 import useProfileImage from "../../assets/images/profileImage.jpeg"
 import {
-  Collapse,
   Navbar,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText
+  Dropdown
 } from 'reactstrap';
 
 export const AppHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(prevState => !prevState);
 
   return (
     <div className="header">
-      <Navbar className="d-flex px-4" expand="md">
+      <Navbar className="d-flex px-4 py-2" expand="md">
+
         <h3>Heelium</h3>
 
-        <div className="user-details">
-            <div className="profile-image-container profile-image-cropper">
+        <Dropdown isOpen={isOpen} toggle={toggle}>
+          <DropdownToggle className="profile-drop-down" color="trans">
+            <div className="user-details">
+              <div className="profile-image-container profile-image-cropper">
                 <img className="profile-image" src={useProfileImage}/>
+              </div>
             </div>
-            <h3 className="user-role">Admin</h3>
-        </div>
-            {/* <NavbarToggler onClick={toggle}/> */}
+            <div className="user-role">Operator</div>
+          </DropdownToggle>
+
+          <DropdownMenu right>
+            <DropdownItem>Another Action</DropdownItem>
+            <DropdownItem>Another Action</DropdownItem>
+            <DropdownItem>Another Action</DropdownItem>
+          </DropdownMenu>
+
+        </Dropdown>
+
       </Navbar>
     </div>
   );
