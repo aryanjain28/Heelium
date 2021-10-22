f6985ed7ece9a403dddd4b65f6d2860f92a1795f
Aryan Jain <aryanjn807@gmail.com>
2021-10-22 23:15:50 +0530

    -> bug-fix

--- src/appConstant.js
+++ src/appConstant.js
@@ -176,52 +176,238 @@ export const data = [
 ]
 
 // Database projected structure
+// As per discussion
 
-// const x = [
+
+
+/**
+ * Inventory Object
+ */
+
+// const  inventory = [
 //     {
-//         productId: "1",
-//         productImage: "NA",
+//         productId: "57900cd0-335b-11ec-8d3d-0242ac130003",
 //         productTitle: "Sock",
-//         productSubCategory: [
-//             {
-//                 id: "ankleSock1",
-//                 name: "Ankle",
-//                 sex: "",    // M, F, "" = Both/Unisex Product
-//                 validColors: ["#fff", "#2fs", "#6fg", "#", ""],
-//                 currentCount: 1120
-//             }, 
-//             {
-//                 id: "stockingSock1",
-//                 name: "Stockings",
-//                 sex: "",    // M, F, 
-//                 validColors: ["#fff", "#2fs", "#6fg", "#", ""],
-//                 currentCount: 76
-//             },
-//             {
-//                 id: "canvasSock1",
-//                 name: "Canvas Socks",
-//                 sex: "",    // M, F, 
-//                 validColors: ["#fff", "#2fs", "#6fg", "#", ""],
-//                 currentCount: 1123
-//             },
-//             {
-//                 id: "Sock4",
-//                 name: "Category 4",
-//                 sex: "",    // M, F, 
-//                 validColors: ["#fff", "#2fs", "#6fg", "#", ""],
-//                 currentCount: 34
-//             },
-//             {
-//                 id: "sick5",
-//                 name: "Type of Sock Category 5",
-//                 sex: "",    // M, F, 
-//                 validColors: ["#fff", "#2fs", "#6fg", "#", ""],
-//                 currentCount: 65
-//             },
+//         gender: [
+//             [
+//                 {
+//                     name: "Ankle Socks",
+//                     validColors: [
+//                         {
+//                             id: "9ba9da4a-335b-11ec-8d3d-0242ac130003",
+//                             name: "red",
+//                             count: 100,
+//                         }, 
+//                         {
+//                             id: "ea3a5d53-8930-4650-b7ee-79111dfe0ab9",
+//                             name: "blue",
+//                             count: 60,
+//                         }, 
+//                         {
+//                             id: "f4f36eed-c963-4171-8292-6f14e44eb575",
+//                             name: "green",
+//                             count: 32,
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     name: "Stockings",
+//                     validColors: [
+//                         {
+//                             id: "dc89a874-335b-11ec-8d3d-0242ac130003",
+//                             name: "green",
+//                             count: 523,
+//                         }, 
+//                         {
+//                             id: "082c7204-fbe0-430b-b81c-3136b0da335d",
+//                             name: "blue",
+//                             count: 60,
+//                         }, 
+//                         {
+//                             id: "eb535076-335b-11ec-8d3d-0242ac130003",
+//                             name: "gray",
+//                             count: 32,
+//                         }
+//                     ]
+//                 },
+//             ],
+//             [
+//                 {
+//                     name: "Ankle Socks",
+//                     validColors: [
+//                         {
+//                             id: "3bc375fe-335c-11ec-8d3d-0242ac130003",
+//                             name: "pink",
+//                             count: 55
+//                         },
+//                         {
+//                             id: "27e2e416-335c-11ec-8d3d-0242ac130003",
+//                             name: "red",
+//                             count: 76
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     name: "Loafers",
+//                     validColors: [
+//                         {
+//                             id: "56a24972-335c-11ec-8d3d-0242ac130003",
+//                             name: "pink",
+//                             count: 55
+//                         },
+//                         {
+//                             id: "4cbb7b262-335c-11ec-8d3d-0242ac130003",
+//                             name: "pink",
+//                             count: 61
+//                         }
+//                     ]
+//                 }
+//             ],
 //         ],
-//         createdAt: "",
-//         createdBy: "",
-//         lastUpdateAt: "June 2010",
-//         lastUpdateBy: "",
+//         createdAt: "1634923546",
+//         createdBy: "<USER-ID>",
+//     },
+//     {
+//         productId: "f2b2aec4-335c-11ec-8d3d-0242ac130003",
+//         productTitle: "T-shirts",
+//         gender: [
+//             [
+//                 {
+//                     name: "Half sleeves",
+//                     validColors: [
+//                         {
+//                             id: "9ba9da4a-335b-11ec-8d3d-0242ac130003",
+//                             name: "red",
+//                             count: 100,
+//                         }, 
+//                         {
+//                             id: "ea3a5d53-8930-4650-b7ee-79111dfe0ab9",
+//                             name: "blue",
+//                             count: 60,
+//                         }, 
+//                         {
+//                             id: "f4f36eed-c963-4171-8292-6f14e44eb575",
+//                             name: "green",
+//                             count: 32,
+//                         }
+//                     ]
+//                 },
+//                 {
+//                     name: "Full Sleeves",
+//                     validColors: [
+//                         {
+//                             id: "dc89a874-335b-11ec-8d3d-0242ac130003",
+//                             name: "green",
+//                             count: 523,
+//                         }, 
+//                         {
+//                             id: "082c7204-fbe0-430b-b81c-3136b0da335d",
+//                             name: "blue",
+//                             count: 60,
+//                         }, 
+//                         {
+//                             id: "eb535076-335b-11ec-8d3d-0242ac130003",
+//                             name: "gray",
+//                             count: 32,
+//                         }
+//                     ]
+//                 },
+//             ],
+//             [
+//                 {
+//                     name: "Half sleeves",
+//                     validColors: [
+//                         {
+//                             id: "3bc375fe-335c-11ec-8d3d-0242ac130003",
+//                             name: "pink",
+//                             count: 55
+//                         },
+//                         {
+//                             id: "27e2e416-335c-11ec-8d3d-0242ac130003",
+//                             name: "red",
+//                             count: 76
+//                         }
+//                     ]
+//                 },
+//             ],
+//         ],
+//         createdAt: "1632331546",
+//         createdBy: "<USER-ID>",
 //     },
 // ]
+
+
+// /**
+//  * Logs
+//  */
+
+// const logs =[ 
+// 	{
+// 		userId: "8fa12f12-335d-11ec-8d3d-0242ac130003",
+// 		logs: [
+// 			{
+// 				productid: "f2b2aec4-335c-11ec-8d3d-0242ac130003",
+//                 colorId: "ea3a5d53-8930-4650-b7ee-79111dfe0ab9",
+// 				time: "1629653146",
+// 				transaction: "" // will discuss and update
+// 			},
+//             {
+// 				productid: "f2b2aec4-335c-11ec-8d3d-0242ac130003",
+//                 colorId: "dc89a874-335b-11ec-8d3d-0242ac130003",
+// 				time: "1600795546",
+// 				transaction: "" // will discuss and update
+// 			},
+// 		],
+// 	},
+// 	{
+//         userId: "192aac0b-280b-418c-b0e2-392f55057e68",
+//         logs: [
+//             {
+// 				productid: "f2b2aec4-335c-11ec-8d3d-0242ac130003",
+//                 colorId: "ea3a5d53-8930-4650-b7ee-79111dfe0ab9",
+// 				time: "1629653146",
+// 				transaction: "" // will discuss and update
+// 			},
+//         ]
+// 	}
+// ]
+
+
+// /**
+//  * User Information
+//  */
+
+// const user = [
+// 	{
+// 		id: "192aac0b-280b-418c-b0e2-392f55057e68",
+// 		name: "Aryan Jain",
+// 		email: "aryan@gmail.com",
+// 		mobile: "9479488833",
+// 		role: "ADMIN"
+// 	},
+//     {
+// 		id: "8fa12f12-335d-11ec-8d3d-0242ac130003",
+// 		name: "XYZ",
+// 		email: "xyz@pqr.in",
+// 		mobile: "6483923435",
+// 		role: "OPERATOR"
+// 	},
+//     {
+// 		id: "e3afeeab-8761-4cd5-b6a0-28ac0a23763f",
+// 		name: "Aayush Gattawar",
+// 		email: "aayush@yahoo.com",
+// 		mobile: "9876543211",
+// 		role: "ADMIN",
+// 	},
+// ]
+
+
+
+
+
+
+
+
+
+
+
